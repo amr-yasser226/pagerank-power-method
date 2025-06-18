11a860a2e52b786da45374eeeffe5a06c406e764
Amr Yasser <amr.yasser200.26@gmail.com>
2025-06-18 21:45:00 +0300

    Solving some conflits and merging the branches

--- Deliverable 4 Experiments & Validation/run_real_experiments.py
+++ Deliverable 4 Experiments & Validation/run_real_experiments.py
@@ -6,7 +6,6 @@ from utils import plot_residuals
 G = nx.karate_club_graph().to_directed()
 P = build_transition_matrix(G)
 v = build_teleportation_vector(len(G))
-
 r, residuals, iters, runtime = pagerank_power_method(P, v)
 
 print(f"Karate Club: {iters} iterations, runtime={runtime:.2f}s, Final residual={residuals[-1]:.2e}")
